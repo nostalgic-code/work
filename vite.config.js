97cd7e3690f4a5c7b929aae0e3d5d76585f48452
nostalgic-code <151646857+nostalgic-code@users.noreply.github.com>
2025-06-03 13:49:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  root: ".", // tell Vite that the root is the current directory
   build: {
+    outDir: "dist", // required for Vercel
+    emptyOutDir: true,
+
     assetsInclude: [
       "**/*.jpeg",
       "**/*.jpg",
